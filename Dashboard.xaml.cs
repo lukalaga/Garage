9255b9f7d99711c73249ce2941c14658698349cf
Nickson Lukalaga <nickson811@outlook.com>
2023-04-13 18:13:16 +0300

    Add project files.

---
+++ Dashboard.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace GarageManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMechanic_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new instance of the Mechanic window
+            Mechanics mechanicWindow = new Mechanics();
+            // Hide the Dashboard window
+            Hide();
+            // Show the Mechanic window
+            mechanicWindow.ShowDialog();
+            // After the Mechanic window is closed, show the Dashboard window again
+            Show();
+        }
+    }
+}
